01fbe0a63b49f776154601f76e3a55e03f289be7
Menno van den Ende <menno@level-level.com>
2019-05-10 11:43:18 +0200

    phpcs

--- ll-site-health-filter.php
+++ ll-site-health-filter.php
@@ -3,7 +3,7 @@
 use Clarkson\Filters\SiteHealthFilter;
 
 // Skip loading the site health filter if not in a WordPress context
-if ( ! defined('WPINC') ) {
+if ( ! defined( 'WPINC' ) ) {
 	return;
 }
 ( new SiteHealthFilter() )->register_hooks();
--- src/SiteHealthFilter.php
+++ src/SiteHealthFilter.php
@@ -6,12 +6,11 @@ namespace Clarkson\Filters;
  *
  * @author Level Level <info@level-level.com>
  */
-class SiteHealthFilter
-{
+class SiteHealthFilter {
+
 	private $debug;
 
-	function __construct()
-	{
+	function __construct() {
 		$this->debug = ( defined( 'WP_DEBUG' ) ? WP_DEBUG : false );
 	}
 
@@ -21,32 +20,55 @@ class SiteHealthFilter
 	}
 
 	public function remove_status_tests( $status_tests ) {
+		/**
+		 * We want to prevent the possibility that WordPress adds new sensitive data.
+		 * Therefore we don't use the unset() function explained by wordpress in <https://make.wordpress.org/core/2019/04/25/site-health-check-in-5-2/>
+		 */
+
+		// PHP version
+		unset( $status_tests['direct']['php_version'] );
+
+		// SQL server version
+		unset( $status_tests['direct']['sql_server'] );
+
+		// PHP extionsions
+		unset( $status_tests['direct']['php_extensions'] );
+
+		// Events
+		unset( $status_tests['direct']['scheduled_events'] );
+
+		// background updates
 		unset( $status_tests['async']['background_updates'] );
 
 		return $status_tests;
 	}
 
 	public function remove_debug_info( $debug_info ) {
+		/**
+		 * We want to prevent the possibility that WordPress adds new sensitive data.
+		 * Therefore we don't use the unset() function explained by wordpress in <https://make.wordpress.org/core/2019/04/25/site-health-check-in-5-2/>
+		 */
+
 		// Database Protection
-		unset($debug_info['wp-database']);
+		unset( $debug_info['wp-database'] );
 
 		// WP Constants
-		unset($debug_info['wp-constants']);
+		unset( $debug_info['wp-constants'] );
 
 		// Server Architecture
-		unset($debug_info['wp-server']['fields']);
+		unset( $debug_info['wp-server']['fields'] );
 
 		// Theme absolute path
-		unset($debug_info['wp-active-theme']['fields']['theme_path']);
+		unset( $debug_info['wp-active-theme']['fields']['theme_path'] );
 
 		// Directory Info
-		unset($debug_info['wp-paths-sizes']);
+		unset( $debug_info['wp-paths-sizes'] );
 
 		//Filesystem
-		unset($debug_info['wp-filesystem']);
+		unset( $debug_info['wp-filesystem'] );
 
 		//Media handling
-		unset($debug_info['wp-media']);
+		unset( $debug_info['wp-media'] );
 
 		return $debug_info;
 	}
