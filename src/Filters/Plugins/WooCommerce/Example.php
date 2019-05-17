43597d70a8a97e9153f49c1a0308fa234345559b
Menno van den Ende <menno@level-level.com>
2019-05-17 17:27:31 +0200

    fix function names

--- src/Filters/Plugins/WooCommerce/Example.php
+++ src/Filters/Plugins/WooCommerce/Example.php
@@ -14,9 +14,9 @@ class Example {
 	private $slug = 'example';
 
 	public function register_hooks() {
-		add_action( 'woocommerce_admin_status_content_' . $this->$slug, array( $this, 'set_tabs') );
+		add_action( 'woocommerce_admin_status_content_' . $this->$slug, array( $this, 'set_content') );
 
-		add_filter( 'woocommerce_admin_status_tabs', array( $this, 'set_content' ), 10, 1 );
+		add_filter( 'woocommerce_admin_status_tabs', array( $this, 'set_tabs' ), 10, 1 );
 	}
 
 	public function set_tabs( $tabs ){
